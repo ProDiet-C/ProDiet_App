80ee963e6c4ba2237cabf85f4c6d633dd998f9fc
Atakan <atakanbektas.c@gmail.com>
2023-03-30 23:57:12 +0300

    Food dummy data ekleme işlemi tamamlandı.

--- WFA_ProDiet/WFA_ProDiet.DAL/Migrations/20230330205451_mig1.Designer.cs
+++ WFA_ProDiet/WFA_ProDiet.DAL/Migrations/20230330205451_mig1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WFA_ProDiet.MODELS.Models;
 
@@ -11,9 +12,10 @@ using WFA_ProDiet.MODELS.Models;
 namespace WFA_ProDiet.DAL.Migrations
 {
     [DbContext(typeof(ProDietDBContext))]
-    partial class ProDietDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230330205451_mig1")]
+    partial class mig1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
